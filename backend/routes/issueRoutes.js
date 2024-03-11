048d628c5a24594bd941511a419c7318f02fd4de
Hong Kien Nguyen <nguyenhongkien123tk@gmail.com>
2024-03-11 14:39:46 +1100

    finish issue Routes

---
+++ backend/routes/issueRoutes.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const router=express.Router();
+const issueController=require('../controllers/issueController');
+
+router.route('/')
+    .get(issueController.getAllIssueInfo)
+    .post(issueController.createNewIssue)
+    .put(issueController.updateIssueData)
+router.route('/:id')
+    .get(issueController.getOneIssueInfo)
+    .delete(issueController.deleteIssueById)
